a751dccb4659d0264a4a78f6c35be3415af22728
Roberto S <samuel03sr@ciencias.unam.mx>
2025-02-26 13:27:22 -0600

    fix: correccion de sintaxis

--- src/main/java/com/product/api/service/SvcCategoryImp.java
+++ src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -3,6 +3,7 @@ package com.product.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -27,8 +28,8 @@ public class SvcCategoryImp implements SvcCategory{
 	public ResponseEntity<List<Category>> getCategories(){
 		try {
 			return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK);
-		} catch (DataAccesException e) {
-			System.out.print(e.getLocalizedMessage);
+		} catch (DataAccessException e) {
+			System.out.print(e.getLocalizedMessage());
 			throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Error");
 		}
 	}
