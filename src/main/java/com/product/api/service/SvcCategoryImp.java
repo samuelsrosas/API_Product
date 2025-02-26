ea599d49f582dc1aa6eb5b2f1d5429f91f9cb4e4
RaulLabonne <raul_labonne27@ciencias.unam.mx>
2025-02-26 13:21:46 -0600

    feature: Manejo de excepciones

--- src/main/java/com/product/api/service/SvcCategoryImp.java
+++ src/main/java/com/product/api/service/SvcCategoryImp.java
@@ -3,10 +3,13 @@ package com.product.api.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.product.api.entity.Category;
 import com.product.api.repository.RepoCategory;
+import com.product.exception.*;
 
 /**
  * Clase que implementa la interfaz de servicio SvcCategory
@@ -21,7 +24,12 @@ public class SvcCategoryImp implements SvcCategory{
 	 * Retornamos lo que devuelve el metodo getCategories del repositorio.
 	 */
 	@Override
-	public List<Category> getCategories(){
-		return repo.getCategories();
+	public ResponseEntity<List<Category>> getCategories(){
+		try {
+			return new ResponseEntity<>(repo.getCategories(), HttpStatus.OK);
+		} catch (DataAccesException e) {
+			System.out.print(e.getLocalizedMessage);
+			throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Error");
+		}
 	}
 }
