4f338571880f5d7f26a49ff5819e7301bb581ca3
Roberto S <samuel03sr@ciencias.unam.mx>
2025-03-19 13:53:38 -0600

    implementacion de DtoProductOut

--- src/main/java/com/product/api/dto/out/DtoProductOut.java
+++ src/main/java/com/product/api/dto/out/DtoProductOut.java
@@ -1,5 +1,104 @@
 package com.product.api.dto.out;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
 public class DtoProductOut {
+	
+	@Id
+	private Integer product_id;
+	
+	private String gtin;
+
+	private String product;
+
+	private String description;
+
+	private Float price;
+
+	private Integer stock;
+
+	private Integer category_id;
+	
+	private String category;
+
+	private Integer status;
+
+	public Integer getProduct_id() {
+		return product_id;
+	}
+
+	public void setProduct_id(Integer product_id) {
+		this.product_id = product_id;
+	}
+
+	public String getGtin() {
+		return gtin;
+	}
+
+	public void setGtin(String gtin) {
+		this.gtin = gtin;
+	}
+
+	public String getProduct() {
+		return product;
+	}
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Integer getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(Integer category_id) {
+		this.category_id = category_id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	
 	
 }
