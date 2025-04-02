18a132b7265a2a40106f45a1a8daf361a33a6bdc
Sca.layer <raul_labonne27@ciencias.unam.mx>
2025-04-02 13:15:27 -0600

    feature: Documentacion con Swagger

--- src/main/java/com/product/api/controller/CtrlProduct.java
+++ src/main/java/com/product/api/controller/CtrlProduct.java
@@ -22,26 +22,32 @@ import com.product.api.service.SvcProduct;
 import com.product.common.dto.ApiResponse;
 import com.product.exception.ApiException;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
+@Tag(name = "Product", description = "Catálogo de productos")
 public class CtrlProduct {
 
 	@Autowired
 	SvcProduct svc;
 
 	@GetMapping
+	@Operation(summary = "Consultar productos", description = "Retorna todos los productos registrados en el sistema")
 	public ResponseEntity<List<DtoProductListOut>> getProducts() {
 		return svc.getProducts();
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary = "Consultar producto", description = "Retorna el producto asociado al id en el sistema")
 	public ResponseEntity<DtoProductOut> getProduct(@PathVariable Integer id) {
 		return svc.getProduct(id);
 	}
 
 	@PostMapping
+	@Operation(summary = "Crear productos", description = "Registra un nuevo producto en el sistema")
 	public ResponseEntity<ApiResponse> createProduct(@Valid @RequestBody DtoProductIn in, BindingResult bindingResult) {
 		if (bindingResult.hasErrors())
 			throw new ApiException(HttpStatus.BAD_REQUEST, bindingResult.getFieldError().getDefaultMessage());
@@ -50,6 +56,7 @@ public class CtrlProduct {
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "Actualizar producto", description = "Actualiza la informacion de un producto")
 	public ResponseEntity<ApiResponse> updateProduct(@PathVariable Integer id, @Valid @RequestBody DtoProductIn in,
 			BindingResult bindingResult) {
 		if (bindingResult.hasErrors())
@@ -59,11 +66,13 @@ public class CtrlProduct {
 	}
 
 	@PatchMapping("/{id}/enable")
+	@Operation(summary = "Activar productos", description = "Cambia el status de un producto a disponible")
 	public ResponseEntity<ApiResponse> enableProduct(@PathVariable Integer id) {
 		return svc.enableProduct(id);
 	}
 
 	@PatchMapping("/{id}/disable")
+	@Operation(summary = "Desactivar productos", description = "Cambia el status de un producto a no disponible")
 	public ResponseEntity<ApiResponse> disableProduct(@PathVariable Integer id) {
 		return svc.disableProduct(id);
 	}
